a9dd4592814bcfcbea9024c6792feb2ef44f14fc
Amit <amitkumar03022004@gmail.com>
2023-09-05 03:06:58 +0530

    update screen and text-editor remaining night mode

--- texteditor.js
+++ texteditor.js
@@ -4,3 +4,18 @@ textarea.addEventListener("input", function (){
     para.innerText = textarea.value;
     console.log(textarea.value);
 });
+
+// dark mode javascript start here
+let icon = document.querySelector(".fa-sun");
+let moonIcon = document.querySelector(".fa-moon");
+icon.onclick = function(){
+    document.body.classList.toggle("night-mode");
+    // if(document.body.classList.contains("night-mode")){
+    // icon.style.visibility = "visible";
+    // moonicon.style.visibility = "hidden";
+    // }
+    // else{
+    //     icon.style.visibility = "hidden";
+    // moonicon.style.visibility = "visible";
+    // }
+}
